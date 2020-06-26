4a43893fe8a721264f5bd8cea4536518b49b2240
Riku Kishida <makcul324@gmail.com>
2020-06-26 11:40:31 +0900

    add

--- src/spear.c
+++ src/spear.c
@@ -4,6 +4,12 @@ int n;
 int k;
 int A[100000];
 
+int p(int m){
+  long long int t=0;
+  int i;
+  for(i=0;i<n;i++) t+=(long long int)A[i]/m;
+  return t<k;
+}
 
 int main(){
   int i, lb, ub;
@@ -11,7 +17,12 @@ int main(){
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
-
-
+  lb=0,ub=1e9+1;
+  while(ub-lb>1){
+    int mid=(lb+ub)/2;
+    if(p(mid)) ub=mid;
+    else lb=mid;
+  }
+  printf("%d\n",lb);
   return 0;
 }
--- src/works.c
+++ src/works.c
@@ -4,6 +4,19 @@ int n;
 int k;
 int A[100000];
 
+int p(int m){
+  int i,t=1,s=0,b=1;
+  for(i=0;i<n;i++){
+    if(s+A[i]<=m) s+=A[i];
+    else{
+      s=A[i];
+      if(s>m) b=0;
+      t++;
+    }
+  }
+  if(t>k) b=0;
+  return b;
+}
 
 int main(){
   int i, lb, ub;
@@ -11,7 +24,12 @@ int main(){
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
-
-
+  lb=0,ub=1e9;
+  while(ub-lb>1){
+    int mid=(lb+ub)/2;
+    if(p(mid)) ub=mid;
+    else lb=mid;
+  }
+  printf("%d\n",ub);
   return 0;
 }
